fb41a93183a6e6e6f5286d1cc484b886ad5d3287
Sandrino Di Mattia <contact@sandrino.be>
2016-03-24 02:09:27 +0100

    Remove progress from webpack to fix heroku deployments

--- server/rules/authorize.js
+++ server/rules/authorize.js
@@ -1,36 +1,27 @@
 function (user, context, callback) {
   var EXTENSION_URL = 'http://auth0-iam.herokuapp.com';
 
-  // Authorize the user and process the result.
-  authorizeUser(function(err, data) {
-    if (err) {
+  authorizeUser(function(err, res, data) {
+     if (err) {
       return callback(new Error(err));
     }
 
-    // The current application is only accessible by specific groups.
+    console.log(data);
     if (!data.accessGranted) {
       return callback(
         new UnauthorizedError('You are not allowed to access this application.'));
     }
 
     user.groups = data.groups;
-
-    // Persist groups.
-    user.app_metadata = user.app_metadata || {};
-    user.app_metadata.groups = data.groups;
-    return auth0.users.updateAppMetadata(user.user_id, user.app_metadata)
-      .then(function() {
-        callback(null, user, context);
-      })
-      .catch(function(err) {
-        callback(err);
-      });
+    return callback(null, user, context);
   });
 
-  // Call the extension to authorize the user.
   function authorizeUser(cb) {
     request.post({
-      url: EXTENSION_URL + '/authorize/' + user.user_id,
+      url: EXTENSION_URL + '/api/authorize/' + user.user_id,
+      headers: {
+        'x-api-key': 'abc'
+      },
       json: {
         clientId: context.clientID,
         connectionName: context.connection,
