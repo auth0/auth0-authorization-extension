ad93627dac712daa62b7aa63e231ca3fa3852d8f
Sandrino Di Mattia <contact@sandrino.be>
2016-04-08 12:54:54 +0200

    Fix issue with sub when calling from a rule

--- server/lib/middlewares/readSubject.js
+++ server/lib/middlewares/readSubject.js
@@ -3,10 +3,13 @@ import jwtDecode from 'jwt-decode';
 module.exports = function readSubject(req, res, next) {
   try {
     const authorizationHeader = req.get('Authorization') || '';
-    const token = authorizationHeader
-      .replace('Bearer ', '');
-    const decodedToken = jwtDecode(token);
-    req.sub = decodedToken.sub;
+    if (authorizationHeader) {
+      const token = authorizationHeader
+        .replace('Bearer ', '');
+      const decodedToken = jwtDecode(token);
+      req.sub = decodedToken.sub;
+    }
+
     next();
   } catch (e) {
     next(e);
