5067ccd24e20e903598edc30580913bf89042eb5
Sandrino Di Mattia <contact@sandrino.be>
2016-04-08 16:31:25 +0200

    Temp fix to make UI work again

--- server/routes/html.js
+++ server/routes/html.js
@@ -19,7 +19,7 @@ export default () => {
     <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/manage/v0.3.973/css/index.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/styleguide/3.8.4/index.css">
     <% if (assets.style) { %><link rel="stylesheet" type="text/css" href="/app/<%= assets.style %>"><% } %>
-    <% if (assets.version) { %><link rel="stylesheet" type="text/css" href="//cdn.auth0.com/extensions/temp/auth0-authz.ui.<%= assets.version %>.css"><% } %>
+    <% if (assets.version) { %><link rel="stylesheet" type="text/css" href="//cdn.auth0.com/extensions/temp/auth0-groups.ui.<%= assets.version %>.css"><% } %>
   </head>
   <body>
     <div id="app"></div>
@@ -30,8 +30,8 @@ export default () => {
     <% if (assets.vendors) { %><script type="text/javascript" src="/app/<%= assets.vendors %>"></script><% } %>
     <% if (assets.app) { %><script type="text/javascript" src="/app/<%= assets.app %>"></script><% } %>
     <% if (assets.version) { %>
-    <script type="text/javascript" src="//cdn.auth0.com/extensions/temp/auth0-authz.ui.vendors.<%= assets.version %>.js"></script>
-    <script type="text/javascript" src="//cdn.auth0.com/extensions/temp/auth0-authz.ui.<%= assets.version %>.js"></script>
+    <script type="text/javascript" src="//cdn.auth0.com/extensions/temp/auth0-groups.ui.vendors.<%= assets.version %>.js"></script>
+    <script type="text/javascript" src="//cdn.auth0.com/extensions/temp/auth0-groups.ui.<%= assets.version %>.js"></script>
     <% } %>
   </body>
   </html>
