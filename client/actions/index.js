b8168c1cc7a4bd5c200c7d324ec1e1200d6738d9
Sandrino Di Mattia <contact@sandrino.be>
2016-07-25 13:13:59 +0200

    Remove actions that are no longer required

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,5 +1,4 @@
 export * as applicationActions from './application';
-export * as applicationGroupActions from './applicationGroup';
 export * as authActions from './auth';
 export * as configurationActions from './configuration';
 export * as connectionActions from './connection';
@@ -9,10 +8,6 @@ export * as groupMappingActions from './groupMapping';
 export * as groupPickerActions from './groupPicker';
 export * as groupMemberActions from './groupMember';
 export * as logActions from './log';
-export * as permissionActions from './permission';
-export * as roleActions from './role';
 export * as userActions from './user';
 export * as userGroupActions from './userGroup';
-export * as userLogActions from './userLog';
-export * as userDeviceActions from './userDevice';
 export * as userPickerActions from './userPicker';
--- client/actions/user.js
+++ client/actions/user.js
@@ -1,9 +1,7 @@
 import * as constants from '../constants';
 import axios from 'axios';
 
-import { fetchUserLogs } from './userLog';
 import { fetchUserGroups } from './userGroup';
-import { fetchUserDevices } from './userDevice';
 
 /*
  * Search for users.
@@ -80,126 +78,6 @@ export function fetchUser(userId) {
     dispatch(fetchUserDetail(userId, (payload) => {
       dispatch(fetchUserAuthorization(payload.data.user));
     }));
-    dispatch(fetchUserLogs(userId));
     dispatch(fetchUserGroups(userId));
-    dispatch(fetchUserDevices(userId));
-  };
-}
-
-/*
- * Get confirmation to remove MFA from a user.
- */
-export function requestRemoveMultiFactor(user) {
-  return {
-    type: constants.REQUEST_REMOVE_MULTIFACTOR,
-    user
-  };
-}
-
-/*
- * Cancel the removal process.
- */
-export function cancelRemoveMultiFactor() {
-  return {
-    type: constants.CANCEL_REMOVE_MULTIFACTOR
-  };
-}
-
-/*
- * Remove multi factor from a user.
- */
-export function removeMultiFactor(userId, provider) {
-  return (dispatch, getState) => {
-    const userId = getState().mfa.get('userId');
-    dispatch({
-      type: constants.REMOVE_MULTIFACTOR,
-      payload: {
-        promise: axios.delete(`/api/users/${userId}/multifactor/${provider}`, {
-          responseType: 'json'
-        })
-      },
-      meta: {
-        userId
-      }
-    });
-  };
-}
-
-/*
- * Get confirmation to block a user.
- */
-export function requestBlockUser(user) {
-  return {
-    type: constants.REQUEST_BLOCK_USER,
-    user
-  };
-}
-
-/*
- * Cancel blocking a user.
- */
-export function cancelBlockUser() {
-  return {
-    type: constants.CANCEL_BLOCK_USER
-  };
-}
-
-/*
- * Block a user.
- */
-export function blockUser() {
-  return (dispatch, getState) => {
-    const userId = getState().block.get('userId');
-    dispatch({
-      type: constants.BLOCK_USER,
-      payload: {
-        promise: axios.post(`/api/users/${userId}/block`, {
-          timeout: 5000,
-          responseType: 'json'
-        })
-      },
-      meta: {
-        userId
-      }
-    });
-  };
-}
-
-/*
- * Get confirmation to unblock a user.
- */
-export function requestUnblockUser(user) {
-  return {
-    type: constants.REQUEST_UNBLOCK_USER,
-    user
-  };
-}
-
-/*
- * Cancel unblocking a user.
- */
-export function cancelUnblockUser() {
-  return {
-    type: constants.CANCEL_UNBLOCK_USER
-  };
-}
-
-/*
- * Unblock a user.
- */
-export function unblockUser() {
-  return (dispatch, getState) => {
-    const userId = getState().unblock.get('userId');
-    dispatch({
-      type: constants.UNBLOCK_USER,
-      payload: {
-        promise: axios.post(`/api/users/${userId}/unblock`, {
-          responseType: 'json'
-        })
-      },
-      meta: {
-        userId
-      }
-    });
   };
 }
