a37a24086f7ec37ea77af9f5b4f9f496f1831a9a
Ariel Gerstein <ariel.ger@hotmail.com>
2016-08-19 12:08:13 -0300

    Remove manage css, add extension header styles.

--- server/views/index.js
+++ server/views/index.js
@@ -8,7 +8,6 @@ module.exports = `<!DOCTYPE html>
   <link rel="shortcut icon" href="https://cdn.auth0.com/styleguide/4.6.13/lib/logos/img/favicon.png">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/styles/zocial.min.css">
-  <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/manage/v0.3.1672/css/index.min.css">
   <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/styleguide/4.6.13/index.min.css" />
   <% if (assets.style) { %><link rel="stylesheet" type="text/css" href="/app/<%= assets.style %>"><% } %>
   <% if (assets.version) { %><link rel="stylesheet" type="text/css" href="//cdn.auth0.com/extensions/auth0-authz/assets/auth0-authz.ui.<%= assets.version %>.css"><% } %>
