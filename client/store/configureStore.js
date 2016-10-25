7adf5652cfe780092439c65f96991583c0da8409
Daniela Borges <alunassertiva@gmail.com>
2016-10-25 17:08:06 +0100

    add auth0-extension-ui-redux

--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -4,8 +4,7 @@ import promiseMiddleware from 'redux-promise-middleware';
 import { compose, createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../reducers';
-import normalizeErrorMiddleware from '../middlewares/normalizeErrorMiddleware';
-import promiseSuccessMiddleware from '../middlewares/promiseSuccessMiddleware';
+import { promiseSuccessMiddleware, normalizeErrorMiddleware } from 'auth0-extension-ui-redux';
 import DevTools from '../containers/DevTools';
 
 
