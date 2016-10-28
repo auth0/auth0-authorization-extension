187bd700ac6617ac1de49b0b2154c3d95ff38fa0
roman.bazarov <trexbazarov@gmail.com>
2016-10-28 17:29:05 +0300

    delete/update from group details

--- client/components/Users/UserRoles.jsx
+++ client/components/Users/UserRoles.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import { Button, ButtonToolbar } from 'react-bootstrap';
+import  UserRolesDialog from './UserRolesDialog';
 
 class UserRoles extends Component {
+  openModalRoles = () => {
+
+  }
+
   render() {
     return (
       <div>
+        <UserRolesDialog user={this.props.user} onClose={() => {}} />
         <div className="row" style={{ marginBottom: '20px' }}>
           <div className="col-xs-8">
             <p>Roles description</p>
           </div>
           <div className="col-xs-4">
-            <Button className="pull-right" bsStyle="success" onClick={() => {}}>
+            <Button className="pull-right" bsStyle="success" onClick={this.openModalRoles}>
               <i className="icon icon-budicon-473" /> Add role to user
             </Button>
           </div>
