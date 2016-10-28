5a6dade828a75b097f221d8d867bfb8bac4e0e4b
Sandrino Di Mattia <contact@sandrino.be>
2016-10-28 12:25:49 +0200

    Remove cross-env for tests

--- webtask.js
+++ webtask.js
@@ -1,35 +1,9 @@
-'use latest';
+const tools = require('auth0-extension-hapi-tools');
 
-const nconf = require('nconf');
-const Webtask = require('webtask-tools');
+const hapiApp = require('./server');
 const logger = require('./server/lib/logger');
-const { configProvider } = require('./server/lib/config');
 
-logger.info('Starting webtask.');
-
-module.exports = Webtask.fromExpress((req, res) => {
-  nconf
-    .defaults({
-      AUTHORIZE_API_KEY: req.webtaskContext.secrets.EXTENSION_SECRET,
-      AUTH0_DOMAIN: req.webtaskContext.secrets.AUTH0_DOMAIN,
-      AUTH0_CLIENT_ID: req.webtaskContext.secrets.AUTH0_CLIENT_ID,
-      AUTH0_CLIENT_SECRET: req.webtaskContext.secrets.AUTH0_CLIENT_SECRET,
-      AUTH0_SCOPES: req.webtaskContext.secrets.AUTH0_SCOPES,
-      EXTENSION_SECRET: req.webtaskContext.secrets.EXTENSION_SECRET,
-      DATA_CACHE_MAX_AGE: 1000 * 30,
-      NODE_ENV: 'production',
-      HOSTING_ENV: 'webtask',
-      CLIENT_VERSION: process.env.CLIENT_VERSION,
-      USE_OAUTH2: true,
-      WT_URL: req.webtaskContext.secrets.WT_URL
-    });
-
-  // Start the server.
-  const initServer = require('./server');
-
-  const app = initServer({
-    storageContext: req.webtaskContext.storage,
-    configProvider: configProvider.fromWebtaskContext(req.webtaskContext)
-  });
-  return app(req, res);
+module.exports = tools.createServer((req, config, storage) => {
+  logger.info('Starting Authorization Extension - Version:', config('CLIENT_VERSION'));
+  return hapiApp(config, storage);
 });
