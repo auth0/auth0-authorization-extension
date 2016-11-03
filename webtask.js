0805c0b386fe5ecac26de05fe48ca4ac80f6eebf
Sandrino Di Mattia <contact@sandrino.be>
2016-11-03 07:58:59 -0700

    Fixed hapi startup

--- webtask.js
+++ webtask.js
@@ -3,7 +3,7 @@ const tools = require('auth0-extension-hapi-tools');
 const hapiApp = require('./server/init');
 const logger = require('./server/lib/logger');
 
-module.exports = tools.createServer((req, config, storage) => {
+module.exports = tools.createServer((config, storage) => {
   logger.info('Starting Authorization Extension - Version:', config('CLIENT_VERSION'));
   return hapiApp(config, storage);
 });
