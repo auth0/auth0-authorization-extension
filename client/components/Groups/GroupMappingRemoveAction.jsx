b7b3bef00267c1943cc66e95217977a257e4bd89
Daniela Borges <alunassertiva@gmail.com>
2016-11-07 13:52:49 +0000

    fix removal icons (#63)

--- client/components/Groups/GroupMappingRemoveAction.jsx
+++ client/components/Groups/GroupMappingRemoveAction.jsx
@@ -14,7 +14,7 @@ class GroupMappingRemoveAction extends Component {
   render() {
     const { index, loading } = this.props;
     return (
-      <TableAction id={`remove-groupMapping-${index}`} type="success" title="Remove" icon="264"
+      <TableAction id={`remove-groupMapping-${index}`} title="Remove" icon="471"
         onClick={this.onRemove} disabled={loading || false}
       />
     );
