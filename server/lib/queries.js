ebe01df8fc53fb68b50ab61305ac6c8b614f9934
Sandrino Di Mattia <contact@sandrino.be>
2016-11-11 12:07:24 +0100

    Fix last hapi issues

--- server/lib/queries.js
+++ server/lib/queries.js
@@ -4,20 +4,6 @@ import Promise from 'bluebird';
 import memoizer from 'lru-memoizer';
 
 /*
- * Cache applications.
- */
-export const getApplicationsCached = memoizer({
-  load: (db, callback) => {
-    db.getApplications()
-      .then(applications => callback(null, applications))
-      .catch(err => callback(err));
-  },
-  hash: (db) => db.hash || 'applications',
-  max: 100,
-  maxAge: nconf.get('DATA_CACHE_MAX_AGE')
-});
-
-/*
  * Cache connections.
  */
 export const getConnectionsCached = memoizer({
@@ -49,29 +35,6 @@ export const getGroupsCached = memoizer({
   max: 100,
   maxAge: nconf.get('DATA_CACHE_MAX_AGE')
 });
-
-/*
- * Check if access for any of the provided groups is allowed
- */
-export function isApplicationAccessAllowed(db, clientId, userGroups) {
-  const groups = (userGroups || []).map(group => group._id);
-
-  return new Promise((resolve, reject) => {
-    getApplicationsCached(db, (err, apps) => {
-      if (err) {
-        return reject(err);
-      }
-
-      const app = _.find(apps, { _id: clientId });
-      if (!app || !app.groups || app.groups.length === 0) {
-        return resolve(true);
-      }
-
-      const accessAllowed = _.filter(app.groups, (groupId) => groups.indexOf(groupId) >= 0).length > 0;
-      return resolve(accessAllowed);
-    });
-  });
-}
 
 /*
  * Get the full connection names for all mappings.
