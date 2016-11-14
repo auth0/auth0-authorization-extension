f253e77d99070738cf2f586efa7dbd5bb88a450d
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2016-11-14 13:56:48 +0200

    added comment; fixed missed callback

--- server/api/authorize/routes/post_user.js
+++ server/api/authorize/routes/post_user.js
@@ -9,7 +9,6 @@ module.exports = (server) => ({
   config: {
     auth: {
       strategies: [
-        'jwt',
         'extension-secret'
       ]
     },
--- server/lib/compileRule.js
+++ server/lib/compileRule.js
@@ -2,9 +2,11 @@ import ejs from 'ejs';
 import config from './config';
 import authorizeRule from './rules/authorize';
 
-export default (configuration = { }) =>
+export default (configuration = { }, userName = '') =>
   ejs.render(authorizeRule, {
     extensionUrl: config('WT_URL'),
     apiKey: config('AUTHORIZE_API_KEY'),
-    config: configuration
+    updateTime: () => new Date().toISOString(),
+    config: configuration,
+    userName
   });
