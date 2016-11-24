61fd5e72453f16cc1cc2bc7a72dde4197ecb57ca
Sandrino Di Mattia <contact@sandrino.be>
2016-11-24 17:06:45 +0100

    Workaround for environment issue

--- webtask.js
+++ webtask.js
@@ -1,7 +1,17 @@
+const url = require('url');
 const tools = require('auth0-extension-hapi-tools');
 
 const hapiApp = require('./server/init');
 const logger = require('./server/lib/logger');
+
+tools.urlHelpers.getBaseUrl = (req) => {
+  const originalUrl = url.parse(req.originalUrl || '').pathname || '';
+  return url.format({
+    protocol: 'https',
+    host: req.headers.host,
+    pathname: originalUrl.replace(req.path, '').replace(/\/$/g, '')
+  });
+};
 
 module.exports = tools.createServer((config, storage) => {
   logger.info('Starting Authorization Extension - Version:', process.env.CLIENT_VERSION);
