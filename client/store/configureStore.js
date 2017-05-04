c99655a7cbbd7390f5284d72ceccf3cf14486dc5
Sandrino Di Mattia <contact@sandrino.be>
2017-05-04 14:09:57 +0200

    Release 2.4.1

--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -1,4 +1,4 @@
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import promiseMiddleware from 'redux-promise-middleware';
 import { compose, createStore, applyMiddleware } from 'redux';
