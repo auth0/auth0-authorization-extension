a099a4093ba22162fc725b5b008055a16d430f83
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2017-08-04 13:43:07 +0300

    no cache for /policy endpoint

--- server/lib/queries.js
+++ server/lib/queries.js
@@ -313,22 +313,19 @@ export function getUserGroups(db, userId, connectionName, groupMemberships) {
   }
 
   return new Promise((resolve, reject) => {
-    getGroupsCached(db, (err, groups) => {
-      if (err) {
-        return reject(err);
-      }
+    db.getGroups()
+      .then((groups) => {
+        // Get the direct groups memberships of a user.
+        const userGroups = _.filter(groups, (group) => _.includes(group.members, userId));
 
-      // Get the direct groups memberships of a user.
-      const userGroups = _.filter(groups, (group) => _.includes(group.members, userId));
-
-      // Calculate the dynamic user groups based on external and internal group memberships.
-      return getDynamicUserGroups(db, connectionName, [ ...groupMemberships, ...(userGroups.map(g => g.name)) ], groups)
-        .then(dynamicGroups => {
-          const nestedGroups = getParentGroups(groups, _.union(userGroups, dynamicGroups));
-          return resolve(nestedGroups);
-        })
-        .catch(reject);
-    });
+        // Calculate the dynamic user groups based on external and internal group memberships.
+        return getDynamicUserGroups(db, connectionName, [ ...groupMemberships, ...(userGroups.map(g => g.name)) ], groups)
+          .then(dynamicGroups => {
+            const nestedGroups = getParentGroups(groups, _.union(userGroups, dynamicGroups));
+            return resolve(nestedGroups);
+          });
+      })
+      .catch(reject);
   });
 }
 
