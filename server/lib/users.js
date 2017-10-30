cbca85bf02130fbbbe4d2ef6b549e4bb75a6efbf
Vu Minh Thang <vuminhthang.cm@gmail.com>
2017-10-30 17:45:18 +0900

    Get all users in group by 1 request

--- server/lib/users.js
+++ server/lib/users.js
@@ -1,45 +1,27 @@
 import _ from 'lodash';
-import async from 'async';
 import Promise from 'bluebird';
 import apiCall from './apiCall';
 
 export function getUsersById(client, ids, page, limit) {
   return new Promise((resolve, reject) => {
-    const users = [];
     const total = ids.length;
 
     page = (page - 1 < 0) ? 0 : page - 1; // eslint-disable-line no-param-reassign
     ids = ids.splice(page * limit, limit); // eslint-disable-line no-param-reassign
+    if (!ids || ids.length === 0) {
+      return resolve({ total, users: [] });
+    }
+    let idStr = ids.join('" OR "');
+    idStr = '"' + idStr + '"';
+    const queryString = `user_id: (${idStr})`;
 
-    async.eachLimit(ids, 10, (userId, cb) => {
-      apiCall(client, client.users.get, [ { id: userId } ])
-        .then((user) => {
-          users.push(user);
-          cb();
-        })
-        .catch((err) => {
-          if (err && err.statusCode === 404) {
-            users.push({
-              user_id: userId,
-              name: '<User Not Found>',
-              email: userId,
-              identities: [
-                { connection: 'N/A' }
-              ]
-            });
-            return cb();
-          }
-
-          return cb(err);
-        });
-    }, (err) => {
-      if (err) {
-        return reject(err);
-      }
-
+    apiCall(client, client.users.getAll, [ { q: queryString } ])
+    .then(function(users) {
       const sorted = _.sortByOrder(users, 'user_id');
-
       return resolve({ total, users: sorted });
+    })
+    .catch(function(err) {
+      reject(err);
     });
   });
 }
