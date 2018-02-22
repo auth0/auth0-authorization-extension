23040d6396c27ba9607d5cedfdd7c0c90a0ca448
Christian Sakshaug <csakshaug@users.noreply.github.com>
2018-02-22 20:19:08 +0100

    Update permission.js
    Change max name length from 50 to 100 length

--- server/api/permissions/schemas/permission.js
+++ server/api/permissions/schemas/permission.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export default Joi.object().keys({
-  name: Joi.string().min(1).max(50).required(),
+  name: Joi.string().min(1).max(100).required(),
   description: Joi.string().min(1).max(500).required(),
   applicationType: Joi.string().valid('client', 'resource_server').required(),
   applicationId: Joi.string().min(1).max(500).required()
