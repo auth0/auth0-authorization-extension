b8bafa0812ae12d848c11e98d10f74d883f055d0
Nikki Ashton <na@boligportal.dk>
2018-04-03 16:35:28 +0200

    Add basic mongodb storage support

--- server/lib/storage/providers.js
+++ server/lib/storage/providers.js
@@ -1,12 +1,34 @@
 import path from 'path';
 import { S3StorageContext } from 'auth0-extension-s3-tools';
 import { FileStorageContext, WebtaskStorageContext, BlobRecordProvider } from 'auth0-extension-tools';
+import { MongoRecordProvider } from 'auth0-extension-mongo-tools';
 
 import config from '../config';
 import logger from '../logger';
 
 export function createProvider(storageContext) {
   switch (config('STORAGE_TYPE')) {
+    case 'mongodb': {
+      logger.info('Initializing the MongoDB Storage Provider.');
+      const mongoProvider = new MongoRecordProvider(config('MONGODB_URL'));
+      mongoProvider.storageContext = {
+        read: async () => {
+          const groups = mongoProvider.getAll('groups');
+          const roles = mongoProvider.getAll('roles');
+          const permissions = mongoProvider.getAll('permissions');
+
+          const resolved = await Promise.all([groups, roles, permissions]);
+
+          return {
+            groups: resolved[0],
+            roles: resolved[1],
+            permissions: resolved[2]
+          };
+        }
+      };
+
+      return mongoProvider;
+    }
     case 's3': {
       logger.info('Initializing the S3 Storage Context.');
 
