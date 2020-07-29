8883cadb192f2e82949a5b04f44b25a4f1e628ac
6footgeek <andy.barlow@auth0.com>
2020-07-29 14:12:13 +0100

    minify/uglify

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
-const path = require('path');
-const nconf = require('nconf');
+const path = require("path");
+const nconf = require("nconf");
 
-const logger = require('./server/lib/logger');
+const logger = require("./server/lib/logger");
 
 // Initialize babel.
-require('babel-core/register')({
-  ignore: /node_modules/,
-  sourceMaps: !(process.env.NODE_ENV === 'production')
+require("@babel/register")({
+  ignore: [/node_modules/],
+  sourceMaps: !(process.env.NODE_ENV === "production"),
+  plugins: [
+    "@babel/plugin-proposal-export-default-from",
+    "@babel/plugin-proposal-object-rest-spread",
+  ],
+  presets: [
+    [
+      "@babel/env",
+      {
+        targets: {
+          node: "current",
+        },
+      },
+    ],
+  ],
 });
-require('babel-polyfill');
+require("babel-polyfill");
 
 // Initialize configuration.
 nconf
   .argv()
   .env()
-  .file(path.join(__dirname, './server/config.json'))
+  .file(path.join(__dirname, "./server/config.json"))
   .defaults({
-    AUTH0_RTA: 'auth0.auth0.com',
+    AUTH0_RTA: "auth0.auth0.com",
     DATA_CACHE_MAX_AGE: 1000 * 10,
-    NODE_ENV: 'development',
-    HOSTING_ENV: 'default',
+    NODE_ENV: "development",
+    HOSTING_ENV: "default",
     PORT: 3001,
     USE_OAUTH2: false,
-    LOG_COLOR: true
+    LOG_COLOR: true,
   });
 
 // Start the server.
-return require('./server/init')((key) => nconf.get(key), null, (err, hapi) => {
-  if (err) {
-    return logger.error(err);
-  }
+return require("./server/init")(
+  (key) => nconf.get(key),
+  null,
+  (err, hapi) => {
+    if (err) {
+      return logger.error(err);
+    }
 
-  return hapi.start(() => {
-    logger.info('Server running at:', hapi.info.uri);
-  });
-});
+    return hapi.start(() => {
+      logger.info("Server running at:", hapi.info.uri);
+    });
+  }
+);
