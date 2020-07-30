6f4d4f1203e6c5a60141258fff464f1eb1ca2021
hinerman <hinerman@gmail.com>
2020-07-30 12:31:19 -0400

    Fix groups functional tests

--- tests/integration/group.tests.js
+++ tests/integration/group.tests.js
@@ -6,10 +6,9 @@ import expect from 'expect';
 import faker from 'faker';
 import { getAccessToken, authzApi, token } from './utils';
 
-const dataToImportExport = {};
 let accessToken;
-let remoteGroup;
-const groupMemberName = 'auth0|test-user-12345';
+const groupMemberName1 = 'auth0|test-user-12345-1';
+const groupMemberName2 = 'auth0|test-user-12345-2';
 
 const parallelGroups = [ ...new Array(20) ].map(() => ({
   name: faker.lorem.slug(),
@@ -101,7 +100,7 @@ describe('groups', () => {
         expect(response.statusCode).toEqual(204);
       }));
 
-  it.skip('should export data', () =>
+  it('should export data', () =>
     request
       .get({
         url: authzApi('/configuration/export'),
@@ -109,7 +108,8 @@ describe('groups', () => {
         json: true
       })
       .then((data) => {
-        expect(data).toEqual(dataToImportExport);
+        const exportedDataKeys = Object.keys(data);
+        expect(exportedDataKeys).toEqual([ 'groups', 'roles' ]);
       }));
 
   it('should create a new group', () =>
@@ -127,7 +127,6 @@ describe('groups', () => {
         })
     ));
 
-  // Skipped waiting to this function to be implemented in the API.
   it('should create many groups in parallel', () => {
     const creationRequests = parallelGroups.map((groupData) =>
       request.post({
@@ -147,27 +146,22 @@ describe('groups', () => {
     );
   });
 
-  // Get all the groups and delete them all expect one
-  it.skip('should delete groups in parallel', () =>
-    request.get({ url: authzApi('/groups'), headers: token(), json: true }).then((data) => {
-      const groups = data.groups.splice(1);
-      const deletionRequests = groups.map((group) =>
-        request.delete({
-          url: authzApi(`/groups/${group._id}`),
-          headers: token(),
-          resolveWithFullResponse: true
+  it('should delete group', () => request
+      .delete({
+        url: authzApi(`/groups/${testGroup._id}`),
+        headers: token(),
+        resolveWithFullResponse: true
+      })
+      .then(() =>
+        request.get({ url: authzApi(`/groups/${testGroup._id}`), headers: token(), json: true }).then(() => {
+          throw new Error('Should have thrown');
         })
-      );
-
-      // Let's fetch all the groups and find.
-      return Promise.all(deletionRequests).then(() =>
-        request.get({ url: authzApi('/groups'), headers: token(), json: true }).then((data) => {
-          data.groups.forEach((group) => {
-            expect(parallelGroups.find((g) => g._id === group._id)).toNotExist();
-          });
-        })
-      );
-    }));
+      )
+      .catch((error) => {
+        expect(error.error.statusCode).toEqual(400);
+        expect(error.error.error).toEqual('Bad Request');
+        expect(error.error.message).toEqual(`The record ${testGroup._id} in groups does not exist.`);
+      }));
 
   it('should get all groups in the system', () =>
     request.get({ url: authzApi('/groups'), headers: token(), json: true }).then((data) => {
@@ -215,131 +209,176 @@ describe('groups', () => {
       });
   });
 
-  it.skip('should add mappings to a group', () => {
-    const mappings = [ { groupName: 'My groupName', connectionName: 'google-oauth2' } ];
-    return request
-      .patch({
-        url: authzApi(`/groups/${testGroup._id}/mappings`),
-        body: mappings,
-        headers: token(),
-        json: true
-      })
-      .then(() => {
-        request
-          .get({
-            url: authzApi(`/groups/${testGroup._id}/mappings`),
-            headers: token(),
-            json: true
-          })
-          .then((groupMappings) => {
-            expect(groupMappings.length).toEqual(1);
-            expect(groupMappings[0].groupName).toEqual('My groupName');
-            expect(groupMappings[0].connectionName).toEqual('google-oauth2 (google-oauth2)');
-          });
-      });
+  describe('mappings', () => {
+    beforeEach(() => {
+      const mappings = [ { groupName: 'My groupName', connectionName: 'google-oauth2' } ];
+      return request
+        .patch({
+          url: authzApi(`/groups/${testGroup._id}/mappings`),
+          body: mappings,
+          headers: token(),
+          json: true
+        })
+        .then(() => {
+          request
+            .get({
+              url: authzApi(`/groups/${testGroup._id}/mappings`),
+              headers: token(),
+              json: true
+            })
+            .then((groupMappings) => {
+              expect(groupMappings.length).toEqual(1);
+              expect(groupMappings[0].groupName).toEqual('My groupName');
+              expect(groupMappings[0].connectionName).toEqual('google-oauth2 (google-oauth2)');
+            });
+        });
+    });
+
+    it('should add mappings to a group', () => {
+      const mappings = [ { groupName: 'My groupName2', connectionName: 'google-oauth2' } ];
+      return request
+        .patch({
+          url: authzApi(`/groups/${testGroup._id}/mappings`),
+          body: mappings,
+          headers: token(),
+          json: true
+        })
+        .then(() => {
+          request
+            .get({
+              url: authzApi(`/groups/${testGroup._id}/mappings`),
+              headers: token(),
+              json: true
+            })
+            .then((groupMappings) => {
+              expect(groupMappings.length).toEqual(2);
+              expect(groupMappings[1].groupName).toEqual('My groupName2');
+              expect(groupMappings[1].connectionName).toEqual('google-oauth2 (google-oauth2)');
+            });
+        });
+    });
+
+    it('should get the mappings of a group', () =>
+      request
+        .get({
+          url: authzApi(`/groups/${testGroup._id}/mappings`),
+          headers: token()
+        })
+        .then((response) => {
+          expect(response.length).toBeGreaterThan(0);
+        }));
+
+    it('should remove mappings of a group', () => {
+      let mappingCount;
+      return request
+        .get({
+          url: authzApi(`/groups/${testGroup._id}/mappings`),
+          headers: token(),
+          json: true
+        })
+        .then((mappings) => {
+          mappingCount = mappings.length;
+          return mappings[0];
+        })
+        .then((mapping) =>
+          request
+            .delete({
+              url: authzApi(`/groups/${testGroup._id}/mappings`),
+              headers: token(),
+              body: [ mapping._id ],
+              json: true,
+              resolveWithFullResponse: true
+            })
+            .then((res) => {
+              expect(res.statusCode).toEqual(204);
+              return request
+                .get({
+                  url: authzApi(`/groups/${testGroup._id}/mappings`),
+                  headers: token(),
+                  json: true
+                })
+                .then((response) => {
+                  expect(response.length).toEqual(mappingCount - 1);
+                });
+            })
+        );
+    });
   });
 
-  it.skip('should get the mappings of a group', () =>
-    request
-      .get({
-        url: authzApi(`/groups/${testGroup._id}/mappings`),
-        headers: token()
-      })
-      .then((response) => {
-        expect(response.length).toBeGreaterThan(0);
+  describe('group members', () => {
+    beforeEach(() => request
+        .patch({
+          url: authzApi(`/groups/${testGroup._id}/members`),
+          body: [ groupMemberName1 ],
+          headers: token(),
+          json: true
+        }));
+
+    it('should add members to a group', () =>
+      request
+        .patch({
+          url: authzApi(`/groups/${testGroup._id}/members`),
+          body: [ groupMemberName2 ],
+          headers: token(),
+          json: true
+        })
+        .then(() =>
+          request
+            .get({
+              url: authzApi(`/groups/${testGroup._id}/members`),
+              headers: token(),
+              json: true
+            })
+            .then((data) => {
+              expect(data.users.find((member) => member.user_id === groupMemberName2)).toExist();
+            })
+        ));
+
+    it('should get the members of a group', () =>
+      request.get({
+        url: authzApi(`/groups/${testGroup._id}/members`),
+        headers: token(),
+        json: true
       }));
 
-  it.skip('should remove mappings of a group', () => {
-    let mappingCount;
-    return request
-      .get({
-        url: authzApi(`/groups/${testGroup._id}/mappings`),
+    it('should remove members from a group', () =>
+      request
+        .delete({
+          url: authzApi(`/groups/${testGroup._id}/members`),
+          body: [ groupMemberName1 ],
+          headers: token(),
+          json: true
+        })
+        .then(() =>
+          request
+            .get({
+              url: authzApi(`/groups/${testGroup._id}/members`),
+              headers: token(),
+              json: true
+            })
+            .then((data) => {
+              expect(data.users.find((member) => member.user_id === groupMemberName1)).toNotExist();
+            })
+        ));
+
+    it('should get the nested members of a group', () =>
+      request.get({
+        url: authzApi(`/groups/${testGroup._id}/members/nested`),
         headers: token(),
         json: true
-      })
-      .then((mappings) => {
-        mappingCount = mappings.length;
-        return mappings[0];
-      })
-      .then((mapping) =>
-        request
-          .delete({
-            url: authzApi(`/groups/${testGroup._id}/mappings`),
-            headers: token(),
-            body: [ mapping._id ],
-            json: true,
-            resolveWithFullResponse: true
-          })
-          .then((res) => {
-            expect(res.statusCode).toEqual(204);
-            return request
-              .get({
-                url: authzApi(`/groups/${remoteGroup._id}/mappings`),
-                headers: token(),
-                json: true
-              })
-              .then((response) => {
-                expect(response.length).toEqual(mappingCount - 1);
-              });
-          })
-      );
+      }));
   });
 
-  it('should add members to a group', () =>
-    request
+  describe('group roles', () => {
+    beforeEach(() => request
       .patch({
-        url: authzApi(`/groups/${testGroup._id}/members`),
-        body: [ groupMemberName ],
+        url: authzApi(`/groups/${testGroup._id}/roles`),
+        body: [ testRole._id ],
         headers: token(),
         json: true
-      })
-      .then(() =>
-        request
-          .get({
-            url: authzApi(`/groups/${testGroup._id}/members`),
-            headers: token(),
-            json: true
-          })
-          .then((data) => {
-            expect(data.users.find((member) => member.user_id === groupMemberName)).toExist();
-          })
-      ));
+      }));
 
-  it('should get the members of a group', () =>
-    request.get({
-      url: authzApi(`/groups/${testGroup._id}/members`),
-      headers: token(),
-      json: true
-    }));
-
-  it.skip('should remove members from a group', () =>
-    request
-      .delete({
-        url: authzApi(`/groups/${testGroup._id}/members`),
-        body: [ groupMemberName ],
-        headers: token(),
-        json: true
-      })
-      .then(() =>
-        request
-          .get({
-            url: authzApi(`/groups/${testGroup._id}/members`),
-            headers: token(),
-            json: true
-          })
-          .then((data) => {
-            expect(data.users.find((member) => member.user_id === groupMemberName)).toNotExist();
-          })
-      ));
-
-  it('should get the nested members of a group', () =>
-    request.get({
-      url: authzApi(`/groups/${testGroup._id}/members/nested`),
-      headers: token(),
-      json: true
-    }));
-
-  it('should add roles to a group', () =>
+    it('should add roles to a group', () =>
     request
       .patch({
         url: authzApi(`/groups/${testGroup._id}/roles`),
@@ -359,73 +398,43 @@ describe('groups', () => {
           });
       }));
 
-  it.skip('should get the roles of a group', () =>
-    request
-      .get({
-        url: authzApi(`/groups/${testGroup._id}/roles`),
+    it('should get the roles of a group', () =>
+      request
+        .get({
+          url: authzApi(`/groups/${testGroup._id}/roles`),
+          headers: token(),
+          json: true
+        })
+        .then((res) => {
+          expect(res.length).toBeGreaterThan(0);
+        }));
+
+    it('should delete roles from a group', () =>
+      request
+        .delete({
+          url: authzApi(`/groups/${testGroup._id}/roles`),
+          body: [ testRole._id ],
+          headers: token(),
+          json: true
+        })
+        .then(() =>
+          request
+            .get({
+              url: authzApi(`/groups/${testGroup._id}/roles`),
+              headers: token(),
+              json: true
+            })
+            .then((res) => {
+              expect(res.find((role) => role._id === testRole._id)).toNotExist();
+            })
+        ));
+
+    // TODO: What is this?
+    it('should get the nested roles of a group', () =>
+      request.get({
+        url: authzApi(`/groups/${testGroup._id}/roles/nested`),
         headers: token(),
         json: true
-      })
-      .then((res) => {
-        expect(res.length).toBeGreaterThan(0);
       }));
-
-  it.skip('should delete roles from a group', () =>
-    request
-      .delete({
-        url: authzApi(`/groups/${testGroup._id}/roles`),
-        body: [ testRole._id ],
-        headers: token(),
-        json: true
-      })
-      .then(() =>
-        request
-          .get({
-            url: authzApi(`/groups/${testGroup._id}/roles`),
-            headers: token(),
-            json: true
-          })
-          .then((res) => {
-            expect(res.find((role) => role._id === testRole._id)).toNotExist();
-          })
-      ));
-
-  // TODO: What is this?
-  it.skip('should get the nested roles of a group', () =>
-    request.get({
-      url: authzApi(`/groups/${testGroup._id}/roles/nested`),
-      headers: token(),
-      json: true
-    }));
-
-  it('should delete a group', (done) => {
-    request
-      .delete({
-        url: authzApi(`/groups/${testGroup._id}`),
-        headers: token(),
-        resolveWithFullResponse: true
-      })
-      .then(() => {
-        // Check the group was deleted in the server
-        request
-          .get({
-            url: authzApi(`/groups/${testGroup._id}`),
-            headers: token(),
-            json: true
-          })
-          .then((data) => {
-            expect(remoteGroup.name).toNotEqual(data.name);
-            expect(remoteGroup.description).toNotEqual(data.description);
-            done();
-          })
-          .catch((err) => {
-            if (err.statusCode === 400) {
-              done();
-            } else {
-              done(err);
-            }
-          });
-      })
-      .catch(done);
   });
 });
