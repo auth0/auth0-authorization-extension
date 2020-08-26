3c392b96fed1d567a724df5fa04b69aa1ebce14b
Marcos Castany <marcos.castany@auth0.com>
2020-08-25 22:13:13 -0300

    Updated integration tests

--- tests/integration/mocha.js
+++ tests/integration/mocha.js
@@ -5,5 +5,5 @@ process.env.NODE_ENV = 'test';
 
 // Register babel so that it will transpile ES6 to ES5
 // before our tests run.
-require('babel-register')();
+require('@babel/register')();
 require('./runner');
