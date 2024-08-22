ed57a2c56ad388fc26c9c979f490c05fa96c4dff
Tim Saunders <tim.saunders@okta.com>
2024-08-22 15:31:41 +0100

    Fix connections-route unit tests

--- tests/unit/server/connections-route.tests.js
+++ tests/unit/server/connections-route.tests.js
@@ -4,22 +4,25 @@ import { getServerData } from '../server';
 import { getToken } from '../mocks/tokens';
 
 describe('connections-route', async () => {
-  const { server } = await getServerData();
+  let server = null;
+
+  before(async () => {
+    const result = await getServerData();
+    server = result.server;
+  });
 
   describe('#get', () => {
-    it('should return 401 if no token provided', (cb) => {
+    it('should return 401 if no token provided', async () => {
       const options = {
         method: 'GET',
         url: '/api/connections'
       };
 
-      server.inject(options, (response) => {
-        expect(response.result.statusCode).to.be.equal(401);
-        cb();
-      });
+      const response = await server.inject(options);
+      expect(response.result.statusCode).to.be.equal(401);
     });
 
-    it('should return 403 if scope is missing (list connections)', (cb) => {
+    it('should return 403 if scope is missing (list connections)', async () => {
       const token = getToken();
       const options = {
         method: 'GET',
@@ -29,13 +32,11 @@ describe('connections-route', async () => {
         }
       };
 
-      server.inject(options, (response) => {
-        expect(response.result.statusCode).to.be.equal(403);
-        cb();
-      });
+      const response = await server.inject(options);
+      expect(response.result.statusCode).to.be.equal(403);
     });
 
-    it('should return list of connections', (cb) => {
+    it('should return list of connections', async () => {
       const token = getToken('read:connections');
       auth0.get('/api/v2/connections', [ { id: 'cid', name: 'my-connection' } ]);
       const options = {
@@ -46,12 +47,10 @@ describe('connections-route', async () => {
         }
       };
 
-      server.inject(options, (response) => {
-        expect(response.result).to.be.a('array');
-        expect(response.result.length).to.equal(1);
-        expect(response.result[0].id).to.equal('cid');
-        cb();
-      });
+      const response = await server.inject(options);
+      expect(response.result).to.be.a('array');
+      expect(response.result.length).to.equal(1);
+      expect(response.result[0].id).to.equal('cid');
     });
   });
 });
